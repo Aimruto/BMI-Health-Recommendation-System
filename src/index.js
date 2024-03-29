4899d14ae9d373413937b8ec8183c1bca9e81b27
Aiman <hasibaiman@gmail.com>
2024-03-29 23:53:40 +0530

    Adding backend
    adding flask restful api

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import '@fortawesome/fontawesome-free/css/fontawesome.min.css';
